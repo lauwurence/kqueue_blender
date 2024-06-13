020e38b7103f1fa2caf1245a200070d44ac18136
lauwurence <46109734+lauwurence@users.noreply.github.com>
2024-06-13 19:12:28 +0400

    Delete Blender Queue directory

--- Blender Queue/config.py
+++
@@ -1,7 +0,0 @@
-
-TITLE = "kQueue Blender"
-APPID = 'keyclap.kqueue_blender.1'
-ICON = "icons/icon.png"
-RENDER_START_AUDIO = "audio/render_started.ogg"
-RENDER_STOP_AUDIO = "audio/render_stopped.ogg"
-RENDER_FINISH_AUDIO = "audio/render_finished.ogg"
--- Blender Queue/test.py
+++
@@ -1,7 +0,0 @@
-
-import re
-
-# line = 'blender --background "I:/test 1.blend" --scene "Scene" -E "CYCLES" --python "F:/render_settings.py" -f "1,2,3,4,5,6,7,8,9,10"'
-line = "Saved: 'F:\RenPy\00_Renders\input\parts0004.png'"
-# line = 'Saved: "F:\RenPy\00_Renders\input\parts0004.png"'
-print(re.search(r'Saved: [\'|"](.*?)[\'|"]', line).group(1))
