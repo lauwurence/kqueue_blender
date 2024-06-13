d526e22cc60cc19a308bb80eba7d56847baf6047
lauwurence <46109734+lauwurence@users.noreply.github.com>
2024-06-13 19:17:28 +0400

    Update config.py

--- kQueue Blender/config.py
+++ kQueue Blender/config.py
@@ -1,6 +1,8 @@
 
 TITLE = "kQueue Blender"
-APPID = 'keyclap.kqueue_blender.1'
+DEVELOPER = 'keyclap'
+VERSION = (0, 1)
+APPID = f'{DEVELOPER}.{TITLE.replace(" ", "_")}.{".".join([str(v) for v in VERSION])}'.lower()
 ICON = "icons/icon.png"
 RENDER_START_AUDIO = "audio/render_started.ogg"
 RENDER_STOP_AUDIO = "audio/render_stopped.ogg"
